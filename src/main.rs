355a592eff6c46081b29e5534f8230d32bd1400f
Jocelyn Turcotte <jocelyn.turcotte@here.com>
2024-12-21 20:44:57 +0100

    Smoother scrolling by setting the offset in the render thread

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use std::time::{Duration, Instant};
 use std::{borrow::Cow, sync::Arc};
 use wgpu::util::DeviceExt;
 use wgpu::BufferUsages;
-use wgpu::ShaderStages;
 use winit::{
     application::ApplicationHandler,
     event::WindowEvent,
@@ -13,7 +12,7 @@ use winit::{
 };
 
 const AUDIO_BUFFER_SIZE: usize = 512;
-const VERTEX_BUFFER_SIZE: usize = AUDIO_BUFFER_SIZE * 512;
+const VERTEX_BUFFER_SIZE: usize = AUDIO_BUFFER_SIZE * 128;
 
 struct LoopState {
     // See https://docs.rs/winit/latest/winit/changelog/v0_30/index.html#removed
@@ -23,6 +22,7 @@ struct LoopState {
     // the even loop starts running.
     state: Option<InitializedLoopState>,
     last_frame_time: Instant,
+    last_fps_dump_time: Instant,
     frame_count: u32,
 }
 
@@ -31,6 +31,7 @@ impl LoopState {
         LoopState {
             state: None,
             last_frame_time: Instant::now(),
+            last_fps_dump_time: Instant::now(),
             frame_count: 0,
         }
     }
@@ -53,6 +54,7 @@ struct InitializedLoopState {
     bind_group: wgpu::BindGroup,
     config: wgpu::SurfaceConfiguration,
     _stream: Option<cpal::Stream>,
+    audio_sample_rate: f32,
 }
 
 #[repr(C)]
@@ -76,20 +78,6 @@ impl Vertex {
                 offset: 0,
                 shader_location: 0,
                 format: wgpu::VertexFormat::Float32x2,
-            }],
-        }
-    }
-}
-
-impl YValue {
-    fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
-        wgpu::VertexBufferLayout {
-            array_stride: std::mem::size_of::<YValue>() as wgpu::BufferAddress,
-            step_mode: wgpu::VertexStepMode::Instance,
-            attributes: &[wgpu::VertexAttribute {
-                offset: 0,
-                shader_location: 1,
-                format: wgpu::VertexFormat::Float32,
             }],
         }
     }
@@ -282,81 +270,76 @@ impl InitializedLoopState {
         let y_value_offset = Arc::new(Mutex::new(0));
         let y_value_offset_clone = y_value_offset.clone();
         let arc_y_value_offset_buffer = Arc::new(y_value_offset_buffer);
-        let arc_y_value_offset_buffer_clone = arc_y_value_offset_buffer.clone();
 
         // List all cpal input sources
         let host = cpal::default_host();
-        let devices = host.input_devices().unwrap();
-        for device in devices {
-            println!("Input device: {}", device.name().unwrap());
-        }
+        // let devices = host.input_devices().unwrap();
+        // for device in devices {
+        //     println!("Input device: {}", device.name().unwrap());
+        // }
 
         // Print all input configs for the default input device
-        let stream = if let Some(default_input_device) = host.default_input_device() {
-            println!(
-                "Default input device: {}",
-                default_input_device.name().unwrap()
-            );
-            let configs = default_input_device.supported_input_configs().unwrap();
-            for config in configs {
-                println!("Supported input config: {:?}", config);
-            }
+        let (stream, audio_sample_rate) =
+            if let Some(default_input_device) = host.default_input_device() {
+                println!(
+                    "Default input device: {}",
+                    default_input_device.name().unwrap()
+                );
+                // let configs = default_input_device.supported_input_configs().unwrap();
+                // for config in configs {
+                //     println!("Supported input config: {:?}", config);
+                // }
 
-            // Modify the data_callback to update the vertex buffer directly and trigger a redraw
-            let default_config = default_input_device.default_input_config().unwrap();
-            let stream: cpal::Stream = default_input_device
-                .build_input_stream(
-                    &cpal::StreamConfig {
-                        buffer_size: cpal::BufferSize::Fixed(AUDIO_BUFFER_SIZE as u32),
-                        ..default_config.into()
-                    },
-                    move |data: &[f32], _: &cpal::InputCallbackInfo| {
-                        let y_values: Vec<YValue> =
-                            data.iter().map(|&sample| YValue { y: sample }).collect();
-                        let mut y_value_offset = y_value_offset_clone.lock().unwrap();
-                        let offset = (*y_value_offset + y_values.len()) % VERTEX_BUFFER_SIZE;
+                // Modify the data_callback to update the vertex buffer directly and trigger a redraw
+                let default_config = default_input_device.default_input_config().unwrap();
+                let audio_sample_rate = default_config.sample_rate().0 as f32;
+                let stream: cpal::Stream = default_input_device
+                    .build_input_stream(
+                        &cpal::StreamConfig {
+                            buffer_size: cpal::BufferSize::Fixed(AUDIO_BUFFER_SIZE as u32),
+                            ..default_config.into()
+                        },
+                        move |data: &[f32], _: &cpal::InputCallbackInfo| {
+                            let y_values: Vec<YValue> =
+                                data.iter().map(|&sample| YValue { y: sample }).collect();
+                            // FIXME: This is the "read" offset, but the sample callback and the rendering won't always
+                            // be in sync. So we'd also need a separate "write" offset here to avoid visibly cutting the sound wave
+                            // where we'll be writing.
+                            let y_value_offset = y_value_offset_clone.lock().unwrap();
 
-                        // First pass: write to the end of the buffer
-                        let first_pass_len = VERTEX_BUFFER_SIZE - *y_value_offset;
-                        let first_pass_data = &y_values[..first_pass_len.min(y_values.len())];
-                        arc_queue_clone.write_buffer(
-                            &arc_y_value_buffer_clone,
-                            (*y_value_offset * std::mem::size_of::<YValue>())
-                                as wgpu::BufferAddress,
-                            bytemuck::cast_slice(first_pass_data),
-                        );
-
-                        // Second pass: write to the beginning of the buffer
-                        if first_pass_data.len() < y_values.len() {
-                            let second_pass_data = &y_values[first_pass_data.len()..];
+                            // First pass: write to the end of the buffer
+                            let first_pass_len = VERTEX_BUFFER_SIZE - *y_value_offset;
+                            let first_pass_data = &y_values[..first_pass_len.min(y_values.len())];
                             arc_queue_clone.write_buffer(
                                 &arc_y_value_buffer_clone,
-                                0,
-                                bytemuck::cast_slice(second_pass_data),
+                                (*y_value_offset * std::mem::size_of::<YValue>())
+                                    as wgpu::BufferAddress,
+                                bytemuck::cast_slice(first_pass_data),
                             );
-                        }
 
-                        // Update y_value_offset and write the offset to the uniform buffer
-                        *y_value_offset = offset;
-                        arc_queue_clone.write_buffer(
-                            &arc_y_value_offset_buffer_clone,
-                            0,
-                            bytemuck::cast_slice(&[offset as u32]),
-                        );
+                            // Second pass: write to the beginning of the buffer
+                            if first_pass_data.len() < y_values.len() {
+                                let second_pass_data = &y_values[first_pass_data.len()..];
+                                arc_queue_clone.write_buffer(
+                                    &arc_y_value_buffer_clone,
+                                    0,
+                                    bytemuck::cast_slice(second_pass_data),
+                                );
+                            }
 
-                        window_clone.request_redraw();
-                    },
-                    move |err| {
-                        eprintln!("Error: {}", err);
-                    },
-                    None,
-                )
-                .unwrap();
-            stream.play().unwrap();
-            Some(stream)
-        } else {
-            None
-        };
+                            window_clone.request_redraw();
+                        },
+                        move |err| {
+                            eprintln!("Error: {}", err);
+                        },
+                        None,
+                    )
+                    .unwrap();
+                stream.play().unwrap();
+                (Some(stream), audio_sample_rate)
+            } else {
+                (None, 0.0)
+            };
 
         InitializedLoopState {
             window,
@@ -375,6 +358,7 @@ impl InitializedLoopState {
             bind_group,
             config,
             _stream: stream,
+            audio_sample_rate,
         }
     }
 }
@@ -412,6 +396,23 @@ impl ApplicationHandler for LoopState {
                     state.window.request_redraw();
                 }
                 WindowEvent::RedrawRequested => {
+                    let now = Instant::now();
+                    let frame_duration = now.duration_since(self.last_frame_time);
+                    self.last_frame_time = now;
+
+                    // Scroll how the y_values are applied onto vertices based on the time between frames.
+                    let mut y_value_offset = state.y_value_offset.lock().unwrap();
+                    *y_value_offset = (*y_value_offset
+                        + (state.audio_sample_rate
+                            * (frame_duration.as_micros() as f32 / 1000000.0))
+                            as usize)
+                        % VERTEX_BUFFER_SIZE;
+                    state.queue.write_buffer(
+                        &state.y_value_offset_buffer,
+                        0,
+                        bytemuck::cast_slice(&[*y_value_offset as u32]),
+                    );
+
                     let frame = state
                         .surface
                         .get_current_texture()
@@ -445,7 +446,6 @@ impl ApplicationHandler for LoopState {
                             });
                         render_pass.set_pipeline(&state.render_pipeline);
                         render_pass.set_vertex_buffer(0, state.vertex_buffer.slice(..));
-                        // render_pass.set_vertex_buffer(1, state.y_value_buffer.slice(..));
                         render_pass.set_bind_group(0, &state.bind_group, &[]);
                         render_pass.draw(0..VERTEX_BUFFER_SIZE as u32, 0..1);
                     }
@@ -455,11 +455,10 @@ impl ApplicationHandler for LoopState {
 
                     // FPS calculation
                     self.frame_count += 1;
-                    let now = Instant::now();
-                    if now.duration_since(self.last_frame_time) >= Duration::from_secs(1) {
+                    if now.duration_since(self.last_fps_dump_time) >= Duration::from_secs(1) {
                         println!("FPS: {}", self.frame_count);
                         self.frame_count = 0;
-                        self.last_frame_time = now;
+                        self.last_fps_dump_time = now;
                     }
                 }
                 WindowEvent::CloseRequested => event_loop.exit(),
